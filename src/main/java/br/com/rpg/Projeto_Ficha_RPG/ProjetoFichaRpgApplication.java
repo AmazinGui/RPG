b35219ff0fcf0532dffd560f9e2ff25572a31cea
Guilherme <guilherme.pontes0209@gmail.com>
2023-06-05 14:49:09 -0300

    Adição os repositórios na aplicação

--- src/main/java/br/com/rpg/Projeto_Ficha_RPG/ProjetoFichaRpgApplication.java
+++ src/main/java/br/com/rpg/Projeto_Ficha_RPG/ProjetoFichaRpgApplication.java
@@ -1,6 +1,8 @@
 package br.com.rpg.Projeto_Ficha_RPG;
 
+import br.com.rpg.Projeto_Ficha_RPG.service.CrudArmas;
 import br.com.rpg.Projeto_Ficha_RPG.service.CrudPoderesAprimorados;
+import br.com.rpg.Projeto_Ficha_RPG.service.CrudPoderesParanormais;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +13,15 @@ import java.util.Scanner;
 public class ProjetoFichaRpgApplication implements CommandLineRunner {
 
 	private CrudPoderesAprimorados crudPoderesAprimorados;
+	private CrudPoderesParanormais crudPoderesParanormais;
+	private CrudArmas crudArmas;
 	private Boolean system = true;
 
-	public ProjetoFichaRpgApplication(CrudPoderesAprimorados crudPoderesAprimorados) {
+	public ProjetoFichaRpgApplication(CrudPoderesAprimorados crudPoderesAprimorados, CrudPoderesParanormais crudPoderesParanormais,
+									  CrudArmas crudArmas) {
 		this.crudPoderesAprimorados = crudPoderesAprimorados;
+		this.crudPoderesParanormais = crudPoderesParanormais;
+		this.crudArmas = crudArmas;
 	}
 
 	public static void main(String[] args) {
@@ -27,16 +34,30 @@ public class ProjetoFichaRpgApplication implements CommandLineRunner {
 			System.out.println("Qual tabela você quer executar?");
 			System.out.println("0 - Sair");
 			System.out.println("1 - Poderes Aprimorados");
+			System.out.println("2 - Poderes Paranormais");
+			System.out.println("3 - Armas");
 
 			Scanner scanner = new Scanner(System.in);
-			int opcao = Integer.parseInt(scanner.nextLine());
-			switch (opcao) {
-				case 1:
-					crudPoderesAprimorados.iniciar(scanner);
-					break;
-				default:
-					system = false;
-					break;
+			String input = scanner.nextLine();
+			if (!input.isEmpty()) {
+				int opcao = Integer.parseInt(input);
+
+				switch (opcao) {
+					case 1:
+						crudPoderesAprimorados.iniciar(scanner);
+						break;
+					case 2:
+						crudPoderesParanormais.iniciar(scanner);
+						break;
+					case 3:
+						crudArmas.iniciar(scanner);
+						break;
+					default:
+						system = false;
+						break;
+				}
+			} else {
+					throw new Exception("Deu merda!");
 			}
 		}
 	}
