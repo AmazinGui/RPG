f9efa56d5de93d5b3837a0e72df21173bbd3a9ef
Guilherme <guilherme.pontes0209@gmail.com>
2023-06-05 14:49:22 -0300

    Método findByPoder

--- src/main/java/br/com/rpg/Projeto_Ficha_RPG/repository/PoderesParanormaisRepository.java
+++ src/main/java/br/com/rpg/Projeto_Ficha_RPG/repository/PoderesParanormaisRepository.java
@@ -2,7 +2,11 @@ package br.com.rpg.Projeto_Ficha_RPG.repository;
 
 import br.com.rpg.Projeto_Ficha_RPG.tabelas.PoderesAprimorados;
 import br.com.rpg.Projeto_Ficha_RPG.tabelas.PoderesParanormais;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PoderesParanormaisRepository extends CrudRepository<PoderesParanormais, Integer> {
+
+    PoderesParanormais findByPoder(String nome);
 }
